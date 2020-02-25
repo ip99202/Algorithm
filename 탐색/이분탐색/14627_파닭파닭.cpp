5b5bb80774ed797e4520ef64158f17b28eeb46f1
ip99202 <ip99202@naver.com>
2020-02-25 14:00:45 +0900

    Update 14627_파닭파닭.cpp

--- 탐색/이분탐색/14627_파닭파닭.cpp
+++ 탐색/이분탐색/14627_파닭파닭.cpp
@@ -1,20 +1,30 @@
-#include <stdio.h>
-#include <string.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+typedef long long ll;
+vector<ll>arr;
+
 int main() {
-	char arr[101] = { 0, };
-	int ch = 0;
-	scanf("%s", arr);
-	int len = strlen(arr);
-	for (int i = 0; i < len; i++) {
-		ch = arr[i];
-		if (ch < 96) {
-			ch += 32;
-		}
-		else {
-			ch -= 32;
-		}
-		arr[i] = ch;
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ll s, c, sum = 0;
+	cin >> s >> c;
+	arr.resize(s + 1);
+	for (int i = 0; i < s; i++) {
+		cin >> arr[i];
+		sum += arr[i];
 	}
-	printf("%s", arr);
+	ll st = 1, end = 1000000000;
+	while (st <= end) {
+		ll mid = (st + end) / 2;
+		int num = 0;
+		for (int i = 0; i < s; i++)
+			num += arr[i] / mid;
+		if (num >= c)
+			st = mid + 1;
+		else
+			end = mid - 1;
+	}
+	cout << sum - end * c;
 	return 0;
 }
