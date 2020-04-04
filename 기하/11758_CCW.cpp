54c86c807a338f43ec8ff4306280cee6c6fcef0e
ip99202 <ip99202@naver.com>
2020-04-04 21:01:18 +0900

    Create 11758_CCW.cpp

---
+++ 기하/11758_CCW.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>	
+#include<math.h>
+using namespace std;
+
+int ccw(int x1, int x2, int x3, int y1, int y2, int y3) {
+	int temp = x1 * y2 + x2 * y3 + x3 * y1;
+	temp = temp - y1 * x2 - y2 * x3 - y3 * x1;
+	if (temp > 0)
+		return 1;
+	else if (temp < 0)
+		return -1;
+	else
+		return 0;
+}
+
+int main() {
+	int x1, x2, x3, y1, y2, y3;
+	scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
+	printf("%d", ccw(x1, x2, x3, y1, y2, y3));
+}
