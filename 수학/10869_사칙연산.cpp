fc09d957ea968fd2b8426d1069e6df065b57ecf5
ip99202 <ip99202@naver.com>
2020-04-29 02:57:28 +0900

    Create 10869_사칙연산.cpp

---
+++ 수학/10869_사칙연산.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d %d", &num1, &num2);
+	printf("%d\n", num1 + num2);
+	printf("%d\n", num1 - num2);
+	printf("%d\n", num1 * num2);
+	printf("%d\n", num1 / num2);
+	printf("%d\n", num1 % num2);
+
+}
