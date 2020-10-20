16e4703f199896356ecbd835f743e7c6fc692af6
ip99202 <ip99202@naver.com>
2020-10-20 22:15:53 +0900

    Create 14652_나는 행복합니다~.cpp

---
+++ 수학/14652_나는 행복합니다~.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n, m, k; cin >> n >> m >> k;
+	cout << k / m << " " << k % m;
+	return 0;
+}
