45bca3ae94e411431d12cd4eac9cb39c7e02b982
ip99202 <ip99202@naver.com>
2020-11-09 23:29:48 +0900

    Create 1927_최소 힙.cpp

---
+++ 구현/자료구조/1927_최소 힙.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+using namespace std;
+priority_queue<int, vector<int>, greater<int>>pq;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int x;
+		scanf("%d", &x);
+		if (x == 0) {
+			if (pq.empty()) {
+				printf("0\n");
+			}
+			else {
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+		}
+		else
+			pq.push(x);
+	}
+}
