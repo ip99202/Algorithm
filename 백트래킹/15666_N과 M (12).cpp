2145e74c83b63ab13729c118362ecc2387562d6a
allthatfabric <beginfill@kakao.com>
2021-05-13 23:10:24 +0900

    Create 15666_N과 M (12).cpp

---
+++ 백트래킹/15666_N과 M (12).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int>v, arr, checkNum;
+int n, m;
+bool check[10];
+
+void go(int cnt, int here) {
+	if (cnt == m) {
+
+		for (int i = 0; i < m; i++) {
+			printf("%d ", v[i]);
+		}
+		printf("\n");
+		return;
+	}
+	int sameCheck[10001] = { 0, };
+	for (int i = here; i <= n; i++) {
+		if (!check[i] && !sameCheck[arr[i]]) {
+			//check[i] = true;
+			v.push_back(arr[i]);
+			sameCheck[arr[i]] = 1;
+			go(cnt + 1, i);
+			v.pop_back();
+			//check[i] = false;
+		}
+	}
+	return;
+}
+
+int main() {
+	scanf("%d%d", &n, &m);
+	arr.resize(n + 1);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	sort(arr.begin(), arr.end());
+	go(0, 1);
+}
