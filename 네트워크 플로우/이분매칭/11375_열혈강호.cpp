9ce266b89ef095291721553d29b6185c0babf2c5
allthatfabric <beginfill@kakao.com>
2021-06-04 23:47:17 +0900

    Create 11375_열혈강호.cpp

---
+++ 네트워크 플로우/이분매칭/11375_열혈강호.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+using namespace std;
+vector<int>arr[1001];
+int visited[1001];
+bool match[1001];
+int n, m, cnt;
+
+bool dfs(int x) {
+	if (match[x]) return false;
+	match[x] = 1;
+	for (int i = 0; i < arr[x].size(); i++) {
+		int temp = arr[x][i];
+		if (!visited[temp] || dfs(visited[temp])) {
+			visited[temp] = x;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	scanf("%d%d", &n, &m);
+	for (int i = 1; i <= n; i++) {
+		int x;
+		scanf("%d", &x);
+		for (int j = 0; j < x; j++) {
+			int y;
+			scanf("%d", &y);
+			arr[i].push_back(y);
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		fill(match, match + 1001, false);
+		if (dfs(i))
+			cnt++;
+	}
+	printf("%d", cnt);
+}
